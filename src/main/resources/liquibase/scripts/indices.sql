bd9c4db453cab1bda782646a65845dbe163287a4
PACKOJIbHUKOB <jordn-5@yandex.ru>
2023-12-15 00:35:01 +0300

    db.changelog-master.yaml  переделан так. по причине после перезагрузки idea, вылитает ошибка liquibase.exception.ChangeLogParseException: classpath:/db/changelog/db.changelog-master.yaml does not exist, переустановка других версий liquibase не помогли

--- src/main/resources/liquibase/scripts/indices.sql
+++ src/main/resources/liquibase/scripts/indices.sql
@@ -1,10 +1,10 @@
 -- liquibase formatted sql
 
--- changeset aspirin:1
+-- changeSet aspirin:1
 CREATE INDEX student_name_index on student (name);
 
 
---changeset aspirin:2
+--changeSet aspirin:2
 CREATE INDEX faculty_name_color_idx on faculty (name,color);
 
 
